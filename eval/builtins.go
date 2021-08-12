304138917e076dbe35ceb424292d653896255b46
Brendan Tang <b@brendantang.net>
2021-08-11 22:06:37 -0400

    Refactor builtins into one file

--- eval/builtins.go
+++ eval/builtins.go
@@ -1,8 +1,11 @@
 package eval
 
 import (
+	"fmt"
 	"github.com/brendantang/naiveconcat/data"
+	"sort"
 )
+
 
 // CoreDict returns a data.Dictionary with word bindings for built-in primitive
 // values.
@@ -48,4 +51,322 @@ func CoreDict() *data.Dictionary {
 			"apply": data.NewProc(apply), // pop x. if x is a quotation, eval each item. otherwise, eval x.
 		},
 	)
+}
+
+
+
+// IO.
+
+
+func say(d *data.Dictionary, s *data.Stack) error {
+	val, err := s.Pop()
+	if err != nil {
+		return err
+	}
+	fmt.Println(val)
+	return nil
+}
+
+
+func showStack(d *data.Dictionary, s *data.Stack) error {
+	fmt.Println(s)
+	return nil
+}
+
+
+func showDict(d *data.Dictionary, s *data.Stack) error {
+	words := make([]string, 0, len(d.Bindings))
+	for w := range d.Bindings {
+		words = append(words, w)
+	}
+	sort.Strings(words)
+	for _, w := range words {
+		fmt.Printf("%s\t%s\n", w, d.Bindings[w])
+	}
+	return nil
+}
+
+
+
+// Math.
+
+
+func add(d *data.Dictionary, s *data.Stack) error {
+	a, err := s.PopType(data.Number)
+	if err != nil {
+		return err
+	}
+	b, err := s.PopType(data.Number)
+	if err != nil {
+		return err
+	}
+	s.Push(data.NewNumber(b.Number + a.Number))
+
+	return nil
+}
+
+
+func subtract(d *data.Dictionary, s *data.Stack) error {
+	a, err := s.PopType(data.Number)
+	if err != nil {
+		return err
+	}
+	b, err := s.PopType(data.Number)
+	if err != nil {
+		return err
+	}
+	s.Push(data.NewNumber(b.Number - a.Number))
+
+	return nil
+}
+
+
+func multiply(d *data.Dictionary, s *data.Stack) error {
+	a, err := s.PopType(data.Number)
+	if err != nil {
+		return err
+	}
+	b, err := s.PopType(data.Number)
+	if err != nil {
+		return err
+	}
+	s.Push(data.NewNumber(a.Number * b.Number))
+
+	return nil
+}
+
+
+func divide(d *data.Dictionary, s *data.Stack) error {
+	a, err := s.PopType(data.Number)
+	if err != nil {
+		return err
+	}
+	b, err := s.PopType(data.Number)
+	if err != nil {
+		return err
+	}
+	s.Push(data.NewNumber(b.Number / a.Number))
+
+	return nil
+}
+
+
+func equal(d *data.Dictionary, s *data.Stack) error {
+	a, err := s.PopType(data.Number)
+	if err != nil {
+		return err
+	}
+	b, err := s.PopType(data.Number)
+	if err != nil {
+		return err
+	}
+	s.Push(data.NewBoolean(b.Number == a.Number))
+
+	return nil
+}
+
+
+
+// Booleans.
+
+
+// not pops a boolean and pushes its negation.
+func not(d *data.Dictionary, s *data.Stack) error {
+	b, err := s.PopType(data.Boolean)
+	if err != nil {
+		return err
+	}
+	s.Push(data.NewBoolean(!b.Bool))
+	return nil
+}
+
+
+// or pops two booleans and pushes TRUE if either of them is TRUE, FALSE
+// otherwise.
+func or(d *data.Dictionary, s *data.Stack) error {
+	a, err := s.PopType(data.Boolean)
+	if err != nil {
+		return err
+	}
+	b, err := s.PopType(data.Boolean)
+	if err != nil {
+		return err
+	}
+	s.Push(data.NewBoolean(b.Bool || a.Bool))
+	return nil
+}
+
+
+// and pops two booleans and pushes TRUE if both of them are TRUE, FALSE
+// otherwise.
+func and(d *data.Dictionary, s *data.Stack) error {
+	a, err := s.PopType(data.Boolean)
+	if err != nil {
+		return err
+	}
+	b, err := s.PopType(data.Boolean)
+	if err != nil {
+		return err
+	}
+	s.Push(data.NewBoolean(b.Bool && a.Bool))
+	return nil
+}
+
+
+// then pops a predicate and a value. If the predicate is TRUE, push the value.
+// Otherwise discard it.
+func then(d *data.Dictionary, s *data.Stack) error {
+	predicate, err := s.PopType(data.Boolean)
+	if err != nil {
+		return err
+	}
+	consequent, err := s.Pop()
+	if err != nil {
+		return err
+	}
+	if predicate.Bool {
+		err := Eval(consequent, d, s)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+
+
+// Quotation manipulation.
+
+
+// length returns the length of the quotation on top of the stack without
+// popping it.
+func length(d *data.Dictionary, s *data.Stack) error {
+	quot, err := s.Peek()
+	if err != nil {
+		return err
+	}
+	if quot.Type != data.Quotation {
+		return data.NewTypeErr(quot, data.Quotation)
+	}
+	s.Push(data.NewNumber(float64(len(quot.Quotation))))
+
+	return nil
+}
+
+
+// lop pops the quotation on top of the stack, then pushes its tail, then pushes
+// its head.
+func lop(d *data.Dictionary, s *data.Stack) error {
+	quot, err := s.PopType(data.Quotation)
+	if err != nil {
+		return err
+	}
+	head, tail := quot.Quotation[0], quot.Quotation[1:]
+	s.Push(data.NewQuotation(tail...))
+	s.Push(head)
+
+	return nil
+}
+
+
+// appendToQuot adds a value to the end of a quotation.
+func appendToQuot(d *data.Dictionary, s *data.Stack) error {
+	quot, err := s.PopType(data.Quotation)
+	if err != nil {
+		return err
+	}
+	val, err := s.Pop()
+	if err != nil {
+		return err
+	}
+	vals := append(quot.Quotation, val)
+	s.Push(data.NewQuotation(vals...))
+	return nil
+}
+
+
+
+// Dictionary manipulation.
+
+
+// let pops a string and the next value, then saves a word named for that string
+// which evaluates to that value.
+func let(d *data.Dictionary, s *data.Stack) error {
+	wordName, err := s.PopType(data.String)
+	if err != nil {
+		return err
+	}
+	definition, err := s.Pop()
+	if err != nil {
+		return err
+	}
+
+	d.Set(wordName.Str, definition)
+	return nil
+}
+
+
+// define pops a string and the next value, then saves a word named for that string which evaluates to a procedure that applies that value.
+func define(d *data.Dictionary, s *data.Stack) error {
+
+	wordName, err := s.PopType(data.String)
+	if err != nil {
+		return err
+	}
+	definition, err := s.Pop()
+	if err != nil {
+		return err
+	}
+	proc := data.NewProc(
+		func(d *data.Dictionary, s *data.Stack) error {
+			s.Push(definition)
+			return apply(d, s)
+		},
+	)
+	d.Set(wordName.Str, proc)
+
+	return nil
+}
+
+
+
+// Stack manipulation.
+
+
+// dup pops a value, then pushes it twice.
+func dup(d *data.Dictionary, s *data.Stack) error {
+	val, err := s.Pop()
+	if err != nil {
+		return err
+	}
+	s.Push(val)
+	s.Push(val)
+	return nil
+}
+
+
+// drop discards the top value on the stack
+func drop(d *data.Dictionary, s *data.Stack) error {
+	_, err := s.Pop()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+
+// lambda pops a value and pushes a procedure that evaluates to that value.
+func lambda(d *data.Dictionary, s *data.Stack) error {
+	head, err := s.Pop()
+	if err != nil {
+		return err
+	}
+	proc := data.NewProc(
+		func(d *data.Dictionary, s *data.Stack) error {
+			s.Push(head)
+			return apply(d, s)
+		},
+	)
+	s.Push(proc)
+	return nil
 }
